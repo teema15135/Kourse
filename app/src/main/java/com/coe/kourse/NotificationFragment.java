e5c89aeebafe88fcf96bd7ebfcb65837a8e2f1fb
chompusama <chadaporn.p@kkumai.com>
2019-04-03 08:20:36 +0700

    Profile and change splash

--- app/src/main/java/com/coe/kourse/NotificationFragment.java
+++ app/src/main/java/com/coe/kourse/NotificationFragment.java
@@ -1,8 +1,10 @@
 package com.coe.kourse;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,9 +12,13 @@ import android.view.ViewGroup;
 
 public class NotificationFragment extends Fragment {
 
+    FloatingActionButton fabReminder;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_notification, container, false);
+        View view = inflater.inflate(R.layout.fragment_notification, container, false);
+
+        return view;
     }
 }
--- app/src/main/java/com/coe/kourse/ProfileFragment.java
+++ app/src/main/java/com/coe/kourse/ProfileFragment.java
@@ -1,5 +1,6 @@
 package com.coe.kourse;
 
+import android.app.Dialog;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,12 +8,50 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class ProfileFragment extends Fragment {
+
+    Button dialogButton;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view =  inflater.inflate(R.layout.fragment_profile, container, false);
+
+        dialogButton = view.findViewById(R.id.profile_btn_person);
+
+        dialogButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Dialog dialog = new Dialog(getActivity());
+                //dialog.setTitle("Add person");
+                dialog.setContentView(R.layout.dialog_profile_person);
+
+                final EditText namePerson = (EditText) dialog.findViewById(R.id.profile_dialog_name);
+                Button buttonCancel = (Button) dialog.findViewById(R.id.profile_btn_cancel);
+                Button buttonOK = (Button) dialog.findViewById(R.id.profile_btn_ok);
+
+                buttonCancel.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+                    }
+                });
+
+                buttonOK.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                    }
+                });
+
+                dialog.show();
+            }
+        });
+
+        return view;
     }
 }
