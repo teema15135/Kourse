da3c1592af8b1923b524494bdd45a546e960d82b
Teema Luksameset <teema.ma.luk@kkumail.com>
2019-04-11 01:00:43 +0700

    Auto stash before merge of "master" and "origin/master"

--- app/src/main/java/com/coe/kourse/LoginActivity.java
+++ app/src/main/java/com/coe/kourse/LoginActivity.java
@@ -18,6 +18,7 @@ import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
@@ -39,6 +40,7 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        FirebaseApp.initializeApp(this);
         mAuth = FirebaseAuth.getInstance();
         mAuthListener = new FirebaseAuth.AuthStateListener() {
             @Override
