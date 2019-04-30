d27cb65507938479bde0503da6dfa062c920113d
Teema Luksameset <teema.ma.luk@kkumail.com>
2019-05-01 05:10:32 +0700

    Animation set

--- app/src/main/java/com/coe/kourse/HomeUserFragment.java
+++ app/src/main/java/com/coe/kourse/HomeUserFragment.java
@@ -5,6 +5,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -44,6 +45,7 @@ public class HomeUserFragment extends Fragment {
 
     private void setViewAttributes() {
         nameTextView.setText(userName);
+        nameTextView.setGravity(Gravity.CENTER);
     }
 
     private void addContents() {
