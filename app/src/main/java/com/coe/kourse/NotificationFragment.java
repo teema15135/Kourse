3125c02f4d352cc5fd6addffa685248538a8f599
chompusama <chadaporn.p@kkumai.com>
2019-05-22 04:52:09 +0700

    Reminder page  (not complete)

--- app/src/main/java/com/coe/kourse/NotificationFragment.java
+++ app/src/main/java/com/coe/kourse/NotificationFragment.java
@@ -1,39 +1,206 @@
 package com.coe.kourse;
 
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.support.v7.app.AppCompatActivity;
+import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.support.v7.widget.Toolbar;
+
+import com.coe.kourse.data.AlarmReminderContract;
+import com.coe.kourse.data.AlarmReminderDbHelper;
 
 import java.util.ArrayList;
 
-public class NotificationFragment extends Fragment {
+import static com.firebase.ui.auth.AuthUI.getApplicationContext;
+
+public class NotificationFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
 
 
     ArrayList<CourseEvent> events;
-
     FloatingActionButton fabReminder;
+
+    private FloatingActionButton mAddReminderButton;
+    private Toolbar mToolbar;
+    AlarmCursorAdapter mCursorAdapter;
+    AlarmReminderDbHelper alarmReminderDbHelper = new AlarmReminderDbHelper(getActivity());
+    ListView reminderListView;
+    ProgressDialog prgDialog;
+    TextView reminderText;
+    private String alarmTitle = "";
+    private static final int VEHICLE_LOADER = 0;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_notification, container, false);
 
-        events = new ArrayList<>();
-        addTestEvents();
 
-        CourseEvent event = events.get(0);
+        mToolbar = (Toolbar) view.findViewById(R.id.toolbar);
+        ((AppCompatActivity)getActivity()).setSupportActionBar(mToolbar);
+        mToolbar.setTitle(R.string.app_name);
+
+
+        reminderListView = (ListView) view.findViewById(R.id.list);
+        reminderText = (TextView) view.findViewById(R.id.reminderText);
+
+
+        View emptyView = view.findViewById(R.id.empty_view);
+        reminderListView.setEmptyView(emptyView);
+
+        mCursorAdapter = new AlarmCursorAdapter(getActivity(), null);
+        reminderListView.setAdapter(mCursorAdapter);
+
+        reminderListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+
+                Intent intent = new Intent(getActivity(), AddReminderActivity.class);
+
+                Uri currentVehicleUri = ContentUris.withAppendedId(AlarmReminderContract.AlarmReminderEntry.CONTENT_URI, id);
+
+                // Set the URI on the data field of the intent
+                intent.setData(currentVehicleUri);
+
+                startActivity(intent);
+
+            }
+        });
+
+
+        mAddReminderButton = (FloatingActionButton) view.findViewById(R.id.fab);
+
+        mAddReminderButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Intent intent = new Intent(v.getContext(), AddReminderActivity.class);
+                //startActivity(intent);
+                addReminderTitle();
+            }
+        });
+
+        LoaderManager.getInstance(this).initLoader(VEHICLE_LOADER, null, this);
 
         return view;
     }
 
-    private void addTestEvents() {
-        CourseEvent c1 = new CourseEvent(/* fill something */);
-        events.add(c1);
+    @Override
+    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+        String[] projection = {
+                AlarmReminderContract.AlarmReminderEntry._ID,
+                AlarmReminderContract.AlarmReminderEntry.KEY_TITLE,
+                AlarmReminderContract.AlarmReminderEntry.KEY_DATE,
+                AlarmReminderContract.AlarmReminderEntry.KEY_TIME,
+                AlarmReminderContract.AlarmReminderEntry.KEY_REPEAT,
+                AlarmReminderContract.AlarmReminderEntry.KEY_REPEAT_NO,
+                AlarmReminderContract.AlarmReminderEntry.KEY_REPEAT_TYPE,
+                AlarmReminderContract.AlarmReminderEntry.KEY_ACTIVE
+
+        };
+
+        return new CursorLoader(getActivity(),   // Parent activity context
+                AlarmReminderContract.AlarmReminderEntry.CONTENT_URI,   // Provider content URI to query
+                projection,             // Columns to include in the resulting Cursor
+                null,                   // No selection clause
+                null,                   // No selection arguments
+                null);                  // Default sort order
+
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+        mCursorAdapter.swapCursor(cursor);
+        if (cursor.getCount() > 0){
+            reminderText.setVisibility(View.VISIBLE);
+        }else{
+            reminderText.setVisibility(View.INVISIBLE);
+        }
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        mCursorAdapter.swapCursor(null);
+
+    }
+
+    public void addReminderTitle(){
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Set Reminder Title");
+
+        final EditText input = new EditText(getActivity());
+        input.setInputType(InputType.TYPE_CLASS_TEXT);
+        builder.setView(input);
+
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if (input.getText().toString().isEmpty()){
+                    return;
+                }
+
+                alarmTitle = input.getText().toString();
+                ContentValues values = new ContentValues();
+
+                values.put(AlarmReminderContract.AlarmReminderEntry.KEY_TITLE, alarmTitle);
+
+                Uri newUri = getActivity().getContentResolver().insert(AlarmReminderContract.AlarmReminderEntry.CONTENT_URI, values);
+
+                restartLoader();
+
+
+                if (newUri == null) {
+                    Toast.makeText(getActivity().getApplicationContext(), "Setting Reminder Title failed", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getActivity().getApplicationContext(), "Title set successfully", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        builder.show();
+    }
+
+    public void restartLoader(){
+        LoaderManager.getInstance(this).restartLoader(VEHICLE_LOADER, null, this);
     }
 }
+
+
+
+//        events = new ArrayList<>();
+//        addTestEvents();
+//        CourseEvent event = events.get(0);
+
+//    private void addTestEvents() {
+//        CourseEvent c1 = new CourseEvent(/* fill something */);
+//        events.add(c1);
+//    }
